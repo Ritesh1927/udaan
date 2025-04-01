a5eb82d5616d60498b4ceccc9845d7f9b6cb5d38
Aditya Singh <adityasinghedneed@gmail.com>
2025-04-01 12:04:28 +0530

    project setup

--- task-hunt/src/common-component/nav/Navbar.js
+++ task-hunt/src/common-component/nav/Navbar.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import "../nav/Navbar.css";
+import Logoimg from "../../assets/Udaan_logo.png";
+import { Link } from "react-router-dom";
+// import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div>
-      <h1>Hey Its Navbar</h1>
-    </div>
+    <nav className="navbar-main-container">
+      <div className="nav-header">
+        <div className="logo-container">
+          <img className="logo-img " src={Logoimg} alt="" />
+        </div>
+        <div>
+          <li>
+            <Link to="/">Home </Link>
+          </li>
+          <li>
+            <Link to="/about">About </Link>
+          </li>
+          <li>
+            <Link to="/service">Home </Link>
+          </li>
+        </div>
+      </div>
+    </nav>
   );
 };
 
--- task-hunt/src/index.js
+++ task-hunt/src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Error from "./common-component/error/Error";
+import About from "./dasboard-pages/About";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<App />}>
+          <Route path="/about" element={<About />}></Route>
+        </Route>
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
